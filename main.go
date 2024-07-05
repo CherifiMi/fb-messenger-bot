5dc781ad4781c6607f1cd0bf6c262c3a7d2266ad
cherifimi <cherifi154949@gmail.com>
2024-07-05 02:31:57 +0100

    azure

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -17,7 +19,6 @@ const (
 	GRAPHQL_URL  = "https://graph.facebook.com/v12.0/me/messages"
 )
 
-// Message represents the structure of the incoming message
 type Message struct {
 	Object string `json:"object"`
 	Entry  []struct {
@@ -38,8 +39,6 @@ type Message struct {
 		} `json:"messaging"`
 	} `json:"entry"`
 }
-
-// SendMessage represents the structure of the outgoing message
 type SendMessage struct {
 	Recipient struct {
 		ID string `json:"id"`
@@ -52,52 +51,59 @@ type SendMessage struct {
 func main() {
 	router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		mode := c.Query("hub.mode")
-		token := c.Query("hub.verify_token")
-		challenge := c.Query("hub.challenge")
-
-		if mode == "subscribe" && token == VERIFY_TOKEN {
-			c.String(http.StatusOK, challenge)
-		} else {
-			c.String(http.StatusForbidden, "Forbidden")
-		}
-	})
-
-	router.POST("/", func(c *gin.Context) {
-		var message Message
-		if err := c.ShouldBindJSON(&message); err != nil {
-			log.Printf("failed to bind JSON: %v", err)
-			c.String(http.StatusBadRequest, "Invalid request")
-			return
-		}
-
-		for _, entry := range message.Entry {
-			for _, messaging := range entry.Messaging {
-				if messaging.Message != nil {
-					sendMessage(messaging.Sender.ID, "Hi")
-				}
-			}
-		}
-
-		c.String(http.StatusOK, "EVENT_RECEIVED")
-		log.Println("EVENT_RECEIVED")
-	})
-
-	router.GET("/mito", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"mito": "nyx"})
-		log.Println("MITOOOOOOOOOOOOOOOOOOOOOOOOOOO LOGS")
-	})
+	router.GET("/", verifyWebhook)
+	router.POST("/", receiveMessage)
+	router.GET("/mito", mitoHandler)
 
 	port := os.Getenv("HTTP_PLATFORM_PORT")
-	//port := "8080"
-
-	// default back to 8080 for local dev
 	if port == "" {
 		port = "8080"
 	}
 
 	router.Run("127.0.0.1:" + port)
+}
+
+func receiveMessage(c *gin.Context) {
+	var message Message
+	if err := c.ShouldBindJSON(&message); err != nil {
+		log.Printf("failed to bind JSON: %v", err)
+		c.String(http.StatusBadRequest, "Invalid request")
+		return
+	}
+	for _, entry := range message.Entry {
+		for _, messaging := range entry.Messaging {
+			if messaging.Message != nil {
+				switch messaging.Message.Text {
+				case "date":
+					sendMessage(messaging.Sender.ID, time.Now().Format("2006-01-02"))
+				case "hi":
+					sendMessage(messaging.Sender.ID, "Hello")
+				default:
+					sendMessage(messaging.Sender.ID, fmt.Sprintf("echo: %q", messaging.Message))
+				}
+			}
+		}
+	}
+
+	c.String(http.StatusOK, "EVENT_RECEIVED")
+	log.Println("EVENT_RECEIVED")
+}
+
+func verifyWebhook(c *gin.Context) {
+	mode := c.Query("hub.mode")
+	token := c.Query("hub.verify_token")
+	challenge := c.Query("hub.challenge")
+
+	if mode == "subscribe" && token == VERIFY_TOKEN {
+		c.String(http.StatusOK, challenge)
+	} else {
+		c.String(http.StatusForbidden, "Forbidden")
+	}
+}
+
+func mitoHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"mito": "nyx"})
+	log.Println("MITOOOOOOOOOOOOOOOOOOOOOOOOOOO LOGS")
 }
 
 func sendMessage(recipientID, text string) {
